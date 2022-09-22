0fbb0d853ea0fbb02977a496c9eba93185440db3
rudkevych <oksana.rudkevych@gmail.com>
2022-09-22 23:40:17 +0200

    feat: added cart component

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,32 @@
+import styles from './Cart.module.css';
+
+const Cart = (props) => {
+  //TODO: change to real selected elements
+  const selectedItems = [
+    { id: 'c1', name: 'Sushi' },
+    { id: 'c2', name: 'Pasta' },
+  ];
+  const cartItems = (
+    <ul className={styles['cart-items']}>
+      {selectedItems.map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <div>
+      {cartItems}
+      <div className={styles.total}>
+        <span>Total Amount</span>
+        <span>$50</span>
+      </div>
+      <div className={styles.actions}>
+        <button className={styles['button--alt']}>Close</button>
+        <button className={styles.button}>Order</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
