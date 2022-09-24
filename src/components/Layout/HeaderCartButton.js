8f66043674570992cb02124e879a55ffb82942a1
rudkevych <oksana.rudkevych@gmail.com>
2022-09-25 01:41:23 +0200

    feat: added animation on item add

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,32 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
   const cartContext = useContext(CartContext);
   const cartItemsAmount = cartContext.items.reduce((pv, item) => {
     return pv + item.amount;
   }, 0);
+
+  const buttonClasses = `${styles.button} ${
+    buttonIsHighlighted ? styles.bump : ''
+  }`;
+  useEffect(() => {
+    if (!cartContext.items.length) return;
+    setButtonIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setButtonIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartContext.items]);
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={buttonClasses} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
